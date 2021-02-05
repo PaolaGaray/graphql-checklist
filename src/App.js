32e49b126994664b193e9ef18a3b5a8389dac666
Paola Garay <garay.angela@gmail.com>
2021-02-05 12:01:52 -0500

    Apollo Client setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+
+const GET_TODOS = gql `
+    query getTodos {
+      todos {
+        done
+        id
+        text
+      }
+}`;
+
 
 function App() {
+
+  const { data, loading, error } = useQuery(GET_TODOS);
+
+  if(loading) return <div>loading...</div>
+  if(error) return <div>error fetchin todos!</div>
+
   return (
     <div>
-     hello there!
+      {data.todos.map(todo => (
+        <p key={todo.id}>
+          <span>
+            {todo.text}
+          </span>
+        </p>
+      ))}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'https://todo-react-graphql.hasura.app/v1/graphql',
+  cache: new InMemoryCache()
+});
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
