b85bc87f7d2f96f23a30564e984e4ce88115706d
Paola Garay <garay.angela@gmail.com>
2021-02-05 13:50:18 -0500

    Add styles

--- src/App.js
+++ src/App.js
@@ -20,14 +20,40 @@ function App() {
   if(error) return <div>error fetchin todos!</div>
 
   return (
-    <div>
-      {data.todos.map(todo => (
-        <p key={todo.id}>
-          <span>
-            {todo.text}
-          </span>
-        </p>
-      ))}
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa4 fl-1">
+      <h1 className="f2-l">
+        GraphQL Checklist {" "}
+        <span role="img" aria-label="Checkmark">
+          ✅
+        </span>
+      </h1>
+      <form className="mb3">
+        <input
+          className="pa2 f4 b--dashed"
+          type='text'
+          placeholder="Write your todo"
+        />
+        <button
+          className="pa2 f4 bg-green"
+          type='submit'>
+          Create
+        </button>
+      </form>
+
+      {/* Todolist */}
+      <div className="flex items-center justify-center flex-column">
+        {data.todos.map(todo => (
+          <p key={todo.id}>
+            <span className="pointer list pa1 f3">
+              {todo.text}
+            </span>
+            <button className="bg-transparent bn f4">
+              <span className="red">&times;</span>
+            </button>
+          </p>
+        ))}
+      </div>
+
     </div>
   );
 }
